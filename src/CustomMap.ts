fba1e3beb2b1cd56013cbec5db4eff9377afbdfd
ismail harmanda <iharmanda@hotmail.com>
2021-10-31 17:48:36 +0300

    Showing Popup Windows

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -23,12 +23,18 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: "Hi there",
+      });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
