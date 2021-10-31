0d06e646b79b593c356e98850f76bb04bfca881b
ismail harmanda <iharmanda@hotmail.com>
2021-10-31 15:41:14 +0300

    add marker

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,6 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
 export class CustomMap {
   private googleMap: google.maps.Map;
   constructor(divId: string) {
@@ -9,4 +12,16 @@ export class CustomMap {
       },
     });
   }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMaker(company: Company): void {}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 // import { User } from "./User";
 // import { Company } from "./Company";
 import { CustomMap } from "./CustomMap";
+import { User } from "./User";
 
-new CustomMap("map");
+const user = new User();
+const customMap = new CustomMap("map");
+
+customMap.addUserMarker(user);
